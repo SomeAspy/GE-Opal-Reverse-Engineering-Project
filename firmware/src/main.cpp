21caa12003a0bd6507c178d1536dadfb3c0d1956
SomeAspy <amb@aspy.dev>
2026-08-23 00:35:25 -0400

    front panel interaction code

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -11,9 +11,10 @@
 #include "HardwareSerial.h"
 #include <Arduino.h>
 #include <EmonLib.h>
+#include <Wire.h>
 
 // Pins
-const int PowerSwitch = 2;
+
 const int BinSwitch = 3;
 const int TankFull = 5;
 const int TankEmpty = 6;
@@ -27,13 +28,59 @@ const int MotorAmmeter = A0;
 // Using analog because the voltage isn't enough to trigger digital high (~1.6v)
 const int IrReceiver = A1;
 
+// Front Panel
+const byte FrontPanel = 0x60;
+
+// Bitmasks sent to Front panel
+
+const byte CleanLed = 0x01;
+const byte LightLed = 0x02;
+const byte PowerLed = 0x04;
+const byte AddWaterLed = 0x08;
+const byte MakingIceLed = 0x10;
+const byte CleaningLed = 0x20;
+const byte DefrostingLed = 0x40;
+const byte WifiLed = 0x80;
+
+// Bitmasks from the front panl
+const byte ButtonReleased = 0x00;
+const byte CleanButton = 0x01;
+const byte LightButton = 0x02;
+const byte PowerButton = 0x03;
+const byte PowerButtonHold = 0x04;
+const byte LightButtonHold = 0x05;
+const byte CleanButtonHold = 0x06;
+
+// Timing & variables
+
+// Calibration factor by gemini, because I hate complex math
+const double AugerAmmeterCalibrationFactor = 5.76;
+// IRM sample count by Gemini, because I can't be bothered.
+const int AugerAmmeterSampleCount = 1480;
+
+const unsigned long pumpTimeout = 120000; // 2m - Filter can drop flow a lot
+const unsigned long compressorTimeout = 300000; // 5m
+
+// From testing the machine usually settles around 0.45A to 0.48A.
+// 0.05 means the compressor is off.
+// Note currentDrawLimit is ignored when the compressor first starts to
+// accomodate inrush current. Tweak as needed.
+const float currentDrawLimit = 0.50;
+const unsigned long augerMotorGracePeriod = 15000; // 15s for inrush to settle
+const unsigned long defrostCycleLength = 600000;   // 10m
+
+const unsigned long binFullDelay = 10000;
+const unsigned long binFullCooldown = 3600000;
+// Don't DDoS the front panel microprocessor
+const unsigned long panelCommunicationDelay = 50;
+
 EnergyMonitor augerMeter;
 
 void setup() {
   Serial.begin(115200);
+  Wire.begin();
 
   // Grounded inputs need to be pulled up
-  pinMode(PowerSwitch, INPUT_PULLUP);
   pinMode(BinSwitch, INPUT_PULLUP);
   pinMode(TankFull, INPUT_PULLUP);
   pinMode(TankEmpty, INPUT_PULLUP);
@@ -50,52 +97,88 @@ void setup() {
   pinMode(UvLed, OUTPUT);
   pinMode(Pump, OUTPUT);
 
-  // Calibration factor by gemini, because I hate complex math
-  augerMeter.current(MotorAmmeter, 5.76);
+  augerMeter.current(MotorAmmeter, AugerAmmeterCalibrationFactor);
 };
+
+// Front panel communication
+unsigned long lastPanelCommunication = 0;
+byte lastButtonPress = 0x00;
+unsigned long errorBlink = 0;
 
 // Water management
 bool pumping = false;
 unsigned long pumpStartedTime = 0;
-const unsigned long pumpTimeout = 120000; // 2m - Filter can drop flow a lot
 
 // Compressor management
 // If the machine is off, assume its been off for a safe amount of time
 unsigned long compressorStopTime = 300001;
 unsigned long compressorStartTime = 0;
-const unsigned long compressorTimeout = 300000; // 5m
 bool isCompressorRunning = false;
 
 // Auger & defrost cycle management
-// From testing the machine usually settles around 0.45A to 0.48A.
-// 0.05 means the compressor is off.
-// Note currentDrawLimit is ignored when the compressor first starts to
-// accomodate inrush current. Tweak as needed.
-const float currentDrawLimit = 0.50;
-const unsigned long augerMotorGracePeriod = 15000; // 15s for inrush to settle
-const unsigned long defrostCycleLength = 600000;   // 10m
 bool defrostCycle = false;
 unsigned long defrostCycleStartTime = 0;
 
 // Bin management
-const unsigned long binFullDelay = 10000;
-const unsigned long binFullCooldown = 3600000;
 unsigned long binCheck = 0;
 unsigned long binLastFullAt = 0;
 bool binFull = false;
 
 // General state initialization
-bool waitingForReset = false;
+bool tankEmptyHalt = false;
+bool isPowered = false;
+bool isLightOn = false;
 
 void loop() {
   // DIGITAL READS ARE INVERTED BECAUSE WE PULL UP!!!
   // Read values for the current cycle
-  const bool isPowered = digitalRead(PowerSwitch) == LOW;
   const bool isTankFull = digitalRead(TankFull) == LOW;
   const bool isTankEmpty = digitalRead(TankEmpty) == LOW;
   const bool isBinInserted = digitalRead(BinSwitch) == LOW;
-  // IRM sample count by Gemini, because I can't be bothered.
-  const double currentDraw = augerMeter.calcIrms(1480);
+
+  const double currentDraw = augerMeter.calcIrms(AugerAmmeterSampleCount);
+
+  if (millis() - lastPanelCommunication > panelCommunicationDelay) {
+    lastPanelCommunication = millis();
+    byte currentLights = 0x00;
+    if (isPowered) {
+      currentLights |= PowerLed;
+    }
+    if (defrostCycle) {
+      currentLights |= DefrostingLed;
+    }
+    if (tankEmptyHalt) {
+      currentLights |= AddWaterLed;
+    }
+    if (isLightOn) {
+      currentLights |= LightLed;
+    }
+    if (isCompressorRunning) {
+      currentLights |= MakingIceLed;
+    }
+
+    Wire.requestFrom(FrontPanel, (byte)1);
+    if (Wire.available()) {
+      byte buttonCode = Wire.read();
+      if (buttonCode != lastButtonPress) {
+        if (buttonCode != ButtonReleased) {
+          switch (buttonCode) {
+          case PowerButton:
+            isPowered = !isPowered;
+            break;
+          case LightButton:
+            isLightOn = !isLightOn;
+            break;
+          }
+        }
+        lastButtonPress = buttonCode;
+      }
+      Wire.beginTransmission(FrontPanel);
+      Wire.write(currentLights);
+      Wire.endTransmission();
+    }
+  }
+
   bool IrReceiving = false;
 
   // IR Receiver needs a moment to register it's state
@@ -114,7 +197,7 @@ void loop() {
   }
 
   // When the machine should be stopped
-  if (!isPowered || waitingForReset || defrostCycle || binFull) {
+  if (!isPowered || tankEmptyHalt || defrostCycle || binFull) {
     Serial.println("Halted");
     digitalWrite(Pump, LOW);
     digitalWrite(Compressor, LOW);
@@ -131,7 +214,7 @@ void loop() {
 
     // If the bin is open we can assume ice was removed
     if (!isBinInserted) {
-      waitingForReset = false;
+      tankEmptyHalt = false;
       binFull = false;
       binCheck = 0;
     }
@@ -153,7 +236,7 @@ void loop() {
   if (pumping && (millis() - pumpStartedTime > pumpTimeout)) {
     // We are probably out of water
     Serial.println("no water?");
-    waitingForReset = true;
+    tankEmptyHalt = true;
     return;
   }
 
